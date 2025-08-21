41cefb4e5b3a5ff746bbd52824e2ca34a3266f5d
Faturrahman Putra <92559381+fptra28@users.noreply.github.com>
2025-08-21 15:44:51 +0700

    Update pivot.php

--- app/Models/pivot.php
+++ app/Models/pivot.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class pivot extends Model
+class Pivot extends Model
 {
     use HasFactory;
 

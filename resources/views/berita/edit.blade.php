01a9c89fae8938b6e6925b890e5551a3d05e7958
Kresno Nugroho <kresnonugroho46@gmail.com>
2026-04-09 09:59:16 +0700

    update:tiny

--- resources/views/berita/edit.blade.php
+++ resources/views/berita/edit.blade.php
@@ -223,8 +223,7 @@
         </div>
 
         {{-- TinyMCE untuk editor teks --}}
-        <script src="https://cdn.tiny.cloud/1/rijrac2uxn06a1q296snq7j1fi420fd29r3lc1o12yzq6fwv/tinymce/7/tinymce.min.js"
-            referrerpolicy="origin"></script>
+       <script src="https://cdn.tiny.cloud/1/xv7eemu094bxfmbh4f4ff4wubpe7jydmpwxj699930in6w7e/tinymce/8/tinymce.min.js" referrerpolicy="origin" crossorigin="anonymous"></script>
         <script>
             tinymce.init({
                 selector: '#content',
